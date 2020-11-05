43256c813352cd1cfd3edd3079b90b3f43752244
HeartyRhombus <kaylahmg@gmail.com>
2020-11-04 17:07:27 -0800

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.create(name: 'Kelly Clarkson', bio: 'American Idol First Season Winner')
+Genre.create(name: "Country")
+Song.create(name: "Because of You", artist_id: 1, genre_id: 1)
